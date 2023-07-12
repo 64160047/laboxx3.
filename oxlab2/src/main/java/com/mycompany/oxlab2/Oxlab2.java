d63ad3ea1f2499772978f2c5389816920b44d33a
Titapon Keawbua <64160047@go.buu.ac.th>
2023-07-12 13:48:53 +0700

    test

---
+++ oxlab2/src/main/java/com/mycompany/oxlab2/Oxlab2.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.oxlab2;
+
+/**
+ *
+ * @author minnie
+ */
+public class Oxlab2 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
