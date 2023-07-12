98c93685eb5857d936426e09c494b49b56176018
Titapon Keawbua <64160047@go.buu.ac.th>
2023-07-12 14:17:17 +0700

    printShowBoard

--- oxlab2/src/main/java/com/mycompany/oxlab2/Oxlab2.java
+++ oxlab2/src/main/java/com/mycompany/oxlab2/Oxlab2.java
@@ -9,13 +9,27 @@ package com.mycompany.oxlab2;
  * @author minnie
  */
 public class Oxlab2 {
-
+    static char[][] board= {{'_', '_', '_'},{'_', '_', '_'},{'_', '_', '_'}};
     public static void main(String[] args) {
        printStartGameOX();
+       printShowBoard();
+       
     }
     private static void  printStartGameOX() {
          System.out.println("WELCOME OX GAME 👀");
-         System.out.print("Start Game ➤➤ (y/n): ");
-        
+         System.out.println("Start Game ➤➤ (y/n): ");   
     }
+    
+    private static void  printShowBoard(){
+        for (int i = 0; i < board.length; i++) {
+            for (int j = 0; j < board[i].length; j++) {
+                System.out.print(board[i][j] + " ");
+
+            }
+            System.out.println("");
+
+        }
+         
+    }
+   
 }
