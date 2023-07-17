7deaa40f1e5ad737734f46d9d32e143040b7608d
Titapon Keawbua <64160047@go.buu.ac.th>
2023-07-17 16:57:38 +0700

    new printstartGameOx

--- oxlab2/src/main/java/com/mycompany/oxlab2/Oxlab2.java
+++ oxlab2/src/main/java/com/mycompany/oxlab2/Oxlab2.java
@@ -1,7 +1,6 @@
 /*
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  */
-
 package com.mycompany.oxlab2;
 
 import java.util.Scanner;
@@ -11,60 +10,35 @@ import java.util.Scanner;
  * @author minnie
  */
 public class Oxlab2 {
-    static char[][] board= {{'_', '_', '_'},{'_', '_', '_'},{'_', '_', '_'}};
-    private static char player = 'X';
-    private static int row;
-     private static int col;
-    public static void main(String[] args) {
-       printStartGameOX();
-       while(true) {
-           printShowBoard();
-       printShowTurn();
-       printInputRowAndCol();
-       switchPlayer();
-           
-       }
-       
-       
-       
-    }
-    private static void  printStartGameOX() {
-         System.out.println("WELCOME OX GAME 👀");
-         System.out.println("Start Game ➤➤ (y/n): ");   
-    }
-    
-    private static void  printShowBoard(){
-        for (int i = 0; i < board.length; i++) {
-            for (int j = 0; j < board[i].length; j++) {
-                System.out.print(board[i][j] + " ");
-
-            }
-            System.out.println("");
-
-        }
-         
-    }
-     private static void printShowTurn() {
-        System.out.println("______________");
-        System.out.println("Next Turn ➤➤ " + player);
-    }
-    private static void  printInputRowAndCol(){
-        Scanner kb = new Scanner(System.in);
-        System.out.print("Input row ➤ ");
-        row = kb.nextInt();  
-        System.out.print("Input column ➤ ");
-        col = kb.nextInt();
-        board[row-1][col-1] = player;
-    }
-    private static void switchPlayer(){
-     
-        if(player == 'X') {
-            player = 'O';
-        }else{
-            player = 'X';
-        }
-        
-    }
-     
+    public static String start;
+    public static String[][] board = {{"_", "_", "_"}, {"_", "_", "_"}, {"_", "_", "_"}};
+    public static String turn;
+    public static int row;
+    public static int col;
+    public static boolean play = false;
+    public static boolean end;
    
+    public static void main(String[] args) {
+        printStartGameOX(); 
+     
+
+    }
+    public static void printStartGameOX() {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("WELCOME OX GAME 👀");
+        System.out.print("Start Game ➤➤ (y/n): ");
+        start = sc.nextLine().toLowerCase();
+        while (!start.equals("n") && !start.equals("y")) {
+            System.out.print("Start Game ➤➤ (y/n): ");
+            start = sc.nextLine().toLowerCase();
+        }
+        if (start.equals("n")) {
+            play = false;
+        } else {
+            play = true;
+        }
+    }
+
+
+    
 }
