90b084a8463b8f78c5862703cbe876bd5829c9c3
Titapon Keawbua <64160047@go.buu.ac.th>
2023-07-17 18:12:00 +0700

    finnish new version xo

--- oxlab2/src/main/java/com/mycompany/oxlab2/Oxlab2.java
+++ oxlab2/src/main/java/com/mycompany/oxlab2/Oxlab2.java
@@ -11,184 +11,219 @@ import java.util.Scanner;
  */
 public class Oxlab2 {
 
-    public static String start;
-    public static String[][] board = {{"_", "_", "_"}, {"_", "_", "_"}, {"_", "_", "_"}};
+   
+    public static boolean start;
+    public static String startgame;
+    public static String[][] table = {{"-", "-", "-"}, {"-", "-", "-"}, {"-", "-", "-"}};
     public static String turn;
     public static int row;
     public static int col;
-    public static boolean play = false;
-    public static boolean end;
+    public static boolean end = false;
+    public static String continute;
 
     public static void main(String[] args) {
-        printStartGameOX();
-        printShowBoard();
-
+        printWelcome();
+        inputStartgame();
+        if (start == false) {
+            System.out.println("---- BYE BYE SEE YAAA 👋🏻 ----");
+            end = true;
+        }
+        while (!end) {
+            printTurn();
+            inputRowCol();
+        }
     }
 
-    public static void printStartGameOX() {
-        Scanner sc = new Scanner(System.in);
+    public static void printWelcome() {
         System.out.println("WELCOME OX GAME 👀");
-        System.out.print("Start Game ➤➤ (y/n): ");
-        start = sc.nextLine().toLowerCase();
-        while (!start.equals("n") && !start.equals("y")) {
-            System.out.print("Start Game ➤➤ (y/n): ");
-            start = sc.nextLine().toLowerCase();
+    }
+
+    public static void inputStartgame() {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Start XO Games? (Y/N) : ");
+        startgame = sc.nextLine().toLowerCase();
+        while (!startgame.equals("y") && !startgame.equals("n")) {
+            System.out.print("Start XO Games? (Y/N) : ");
+            startgame = sc.nextLine().toLowerCase();
         }
-        if (start.equals("n")) {
-            play = false;
+        if (startgame.equals("n")) {
+            start = false;
         } else {
-            play = true;
+            start = true;
+            printTable();
+            inputFirstturn();
         }
     }
 
-    public static void printShowBoard() {
-        for (int i = 0; i < board.length; i++) {
-            for (int j = 0; j < board[i].length; j++) {
-                System.out.print(board[i][j] + " ");
-
+    public static void printTable() {
+        System.out.println(" ----------- ");
+        for (int i = 0; i < table.length; i++) {
+            for (int j = 0; j < table[i].length; j++) {
+                System.out.print("  " + table[i][j] + " ");
             }
             System.out.println();
-
         }
-    }
-
-    public static void showTurn() {
-        System.out.println("_________________________");
-        System.out.println("Next Turn ➤➤ " + turn.toUpperCase());
+        System.out.println(" ----------- ");
 
     }
 
-    public static void printInputRowAndCol() {
+    public static void inputFirstturn() {
         Scanner sc = new Scanner(System.in);
-        System.out.print("Input row ➤ ");
-        row = sc.nextInt();
-        System.out.print("Input column ➤ ");
-        col = sc.nextInt();
-        if (((row > 0 && row < 4) && (col > 0 && col < 4))) {
-
-            if (board[row - 1][col - 1].equals("_")) {
-
-                board[row - 1][col - 1] = turn.toUpperCase();
-
-            } else {
-
-                while ((!(row > 0 && row < 4) && !(col > 0 && col < 4)) && !(board[row - 1][col - 1].equals("_"))) {
-
-                    System.out.println("Please Input Again.");
-                    System.out.print("Input row ➤ ");
-                    row = sc.nextInt();
-                    System.out.print("Input column ➤");
-                    col = sc.nextInt();
-
-                }
-
-                return;
-            }
-
+        System.out.print("Who first player? (X/O) : ");
+        turn = sc.nextLine().toLowerCase();
+        while (!turn.equals("x") && !turn.equals("o")) {
+            System.out.print("Who first player? (X/O) : ");
+            turn = sc.nextLine().toLowerCase();
+        }
+        if (turn.equals("x")) {
+            turn = "x";
+        }
+        if (turn.equals("o")) {
+            turn = "o";
         } else {
             return;
         }
+        System.out.println("--------------------------------");
+
     }
 
-    public static void nextTurn() {
+    public static void printTurn() {
+        System.out.println("Now " + turn.toUpperCase() + " turn");
+    }
+
+    public static void inputRowCol() {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Please input row, column : ");
+        row = sc.nextInt();
+        col = sc.nextInt();
+        if (((row > 0 && row < 4) && (col > 0 && col < 4))) {
+            if (table[row - 1][col - 1].equals("-")) {
+                table[row - 1][col - 1] = turn.toUpperCase();
+
+            } else {
+                System.out.println(" Plese input again ‼️ ");
+                return;
+            }
+            printTable();
+            isWin();
+            changeTurn();
+        } else {
+            System.out.println(" Plese input again ‼️");
+            return;
+        }
+
+    }
+
+    public static void changeTurn() {
         if (turn.equals("x")) {
             turn = "o";
         } else {
             turn = "x";
         }
-
     }
 
-    public static void checkWin() {
-        if (checkRow() || checkColumn() || checkDiagonal()) {
-
-            System.out.println("⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯");
-            System.out.println("  " + turn + " Win 🏆");
-            System.out.println("⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯");
-            if (endGame()) {
+    public static void isWin() {
+        if (checkCol() || checkRow() || checkObliquely()) {
+            System.out.println(" ----- Congratulations ----- ");
+            System.out.println("       Who the winner 🏆  is " + turn.toUpperCase());
+            System.out.println("--------------------------------");
+            if (inputContinute()) {
                 resetTable();
             } else {
-                play = false;
-
+                end = true;
             }
         }
-        if (play == true) {
+        if (end == false) {
             if (checkDraw()) {
-                System.out.println("⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯");
-                System.out.println("     Draw     ");
-                System.out.println("⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯");
-                if (endGame()) {
+                System.out.println("****** Game over ******");
+                System.out.println("  -----    Draw   -----  ");
+                System.out.println("---------------------------");
+                if (inputContinute()) {
                     resetTable();
                 } else {
-                    play = false;
-
+                    end = true;
                 }
             }
         }
+
     }
 
-    public static boolean checkRow() {
-        for (int j = 0; j < board[row - 1].length; j++) {
-            if (!board[row - 1][j].toLowerCase().equals(turn)) {
+    public static boolean checkCol() {
+        for (int i = 0; i < table[row - 1].length; i++) {
+            if (!table[i][col - 1].toLowerCase().equals(turn)) {
                 return false;
             }
         }
         return true;
     }
 
-    public static boolean checkColumn() {
-        for (int j = 0; j < board[0].length; j++) {
-            if (board[0][j].toLowerCase().equals(turn) && board[1][j].toLowerCase().equals(turn) && board[2][j].toLowerCase().equals(turn)) {
-                return true;
+    public static boolean checkRow() {
+        for (int j = 0; j < table[row - 1].length; j++) {
+            if (!table[row - 1][j].toLowerCase().equals(turn)) {
+                return false;
             }
         }
+        return true;
+    }
+
+    public static boolean checkObliquely() {
+        if (row - 1 == col - 1) {
+            for (int i = 0; i < table.length; i++) {
+                if (!table[i][i].toLowerCase().equals(turn)) {
+                    return false;
+                }
+            }
+            return true;
+        }
+        if ((row + col) - 2 == table.length - 1) {
+            for (int i = 0; i < table.length; i++) {
+                if (!table[i][table.length - 1 - i].toLowerCase().equals(turn)) {
+                    return false;
+                }
+            }
+            return true;
+        }
         return false;
     }
 
-    public static boolean checkDiagonal() {
-        if (board[0][0].toLowerCase().equals(turn) && board[1][1].toLowerCase().equals(turn) && board[2][2].toLowerCase().equals(turn)) {
-            return true;
-        }
-
-        if (board[0][2].toLowerCase().equals(turn) && board[1][1].toLowerCase().equals(turn) && board[2][0].toLowerCase().equals(turn)) {
-            return true;
-        }
-
-        return false;
-    }
     public static boolean checkDraw() {
-        for (int i = 0; i < board.length; i++) {
-            for (int j = 0; j < board.length; j++) {
-                if (board[i][j].equals("_")) {
+        for (int i = 0; i < table.length; i++) {
+            for (int j = 0; j < table.length; j++) {
+                if (table[i][j].toLowerCase().equals("-")) {
                     return false;
                 }
             }
         }
         return true;
     }
-    public static boolean endGame() {
+
+    public static boolean inputContinute() {
         Scanner sc = new Scanner(System.in);
-        String again = sc.nextLine().toLowerCase();
-
-        while (!again.equals("n") && !again.equals("y")) {
-
-            System.out.print("| Do you want to Exit 🚪 | (y/n): ");
-            again = sc.nextLine().toLowerCase();
+        System.out.print("Do You want to play again (Y/N) 🚪 : ");
+        continute = sc.next();
+        while (!continute.toLowerCase().equals("n") && !continute.toLowerCase().equals("y")) {
+            System.out.print("Do You want to play again (Y/N) 🚪 : ");
+            continute = sc.next().toLowerCase();
         }
-        if (again.equals("y")) {
+        if (continute.equals("n")) {
+            end = true;
+            System.out.println("    ----- See yaaa 💖 ----- ");
             return false;
         }
         return true;
-
     }
-     public static void resetTable() {
-        for (int i = 0; i < board.length; i++) {
-            for (int j = 0; j < board.length; j++) {
-                board[i][j] = "_";
+
+    public static void resetTable() {
+        for (int i = 0; i < table.length; i++) {
+            for (int j = 0; j < table.length; j++) {
+                table[i][j] = "-";
             }
         }
-        turn = "o";
+
     }
+
+
+
      
 
 }
