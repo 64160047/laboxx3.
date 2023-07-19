9380f09fd485323fa01fd6b9f16e49675a65f3af
Titapon Keawbua <64160047@go.buu.ac.th>
2023-07-19 13:47:14 +0700

    test

---
+++ labox3/src/main/java/com/mycompany/labox3/Labox3.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.labox3;
+
+/**
+ *
+ * @author minnie
+ */
+public class Labox3 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
